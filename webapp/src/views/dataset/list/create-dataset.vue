aca56e00acea3e1b585a7d320f360edd423e19a6
之江实验室 <tianshu@zhejianglab.com>
2020-10-27 16:15:19 +0800

    update changelog

--- webapp/src/views/dataset/list/create-dataset.vue
+++ webapp/src/views/dataset/list/create-dataset.vue
@@ -367,6 +367,7 @@ export default {
       })),
 
     uploadFinished() {
+      if(this.skipUpload) return true;
       return this.uploadStatus && ['success', 'exception'].includes(this.uploadStatus);
     },
   },  
