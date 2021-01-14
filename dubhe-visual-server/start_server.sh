22e49b0bfd91eeaf9118a666c26a0f3654653948
之江实验室 <tianshu@zhejianglab.com>
2021-01-14 14:23:30 +0800

    update visual server

--- dubhe-visual-server/start_server.sh
+++ dubhe-visual-server/start_server.sh
@@ -8,11 +8,11 @@ cd backend/
 nohup python $DIRNAME/backend/main.py --port 9898 >django.log 2>&1 &
 echo 'http 服务启动'
 
-cd parser_service/
+cd ../parser_service/
 nohup python $DIRNAME/parser_service/master.py >server.log 2>&1 &
 echo 'parser 服务启动'
 
-cd service_utils/
+cd ../service_utils/
 nohup python $DIRNAME/service_utils/monitor.py >monitor.log 2>&1 &
 echo 'monitor 服务启动'
 
